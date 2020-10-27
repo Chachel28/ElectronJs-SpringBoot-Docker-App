dba74919743b958380212e5676619874d867d66a
Alejandro <alexgb11@gmail.com>
2020-10-27 16:55:13 +0100

    Primeros pasos de la api

---
+++ Back/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+\
+,src/main/java/net/juanxxiii/Controlador.java,1\9\1936fd548f65482ae695ee8d3578c2f520e91eec
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
--- Back/src/main/java/net/juanxxiii/Controlador.java
+++ Back/src/main/java/net/juanxxiii/Controlador.java
@@ -1,5 +1,6 @@
 package net.juanxxiii;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class Controlador {
 
-    @RequestMapping("/")
-    public String index(){
-        return "hola";
+    @RequestMapping("/clientes/{id}")
+    public String index(@PathVariable("id") String id){
+        return id;
+    }
+
+    @RequestMapping("/clientes")
+    public String index2(){
+        return "Index2";
     }
 }
