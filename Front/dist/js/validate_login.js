b801fd95804b33a75d92c10a1e7a8c1c10657336
Anibalgomezfer <59883735+Anibalgomezfer@users.noreply.github.com>
2020-11-03 17:09:03 +0100

    El validate del login no funciona aún, aprende JavaScript pringao

--- Front/dist/js/validate_login.js
+++ Front/dist/js/validate_login.js
@@ -1,3 +1,17 @@
-function validate_login(){
 
+function validacion() {
+    let email  =  document.getElementById("email-login").value();
+    let password = document.getElementById("password-login").value();
+    if (email != "admin@gmail.com") {
+        alert('Email no registrado')
+        console.log("false")
+        return false;
+    }
+    else if (password != "Contraseña") {
+        alert('Contraseña incorrecta')
+        console.log("false")
+        return false;
+    }
+    console.log(email)
+    return true;
 }
