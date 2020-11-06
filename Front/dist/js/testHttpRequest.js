19947933b088ae8ff7224f0031a8678ed7c9df1d
Pablo Viniegra <pablovpmadrid@gmail.com>
2020-11-06 17:48:08 +0100

    Se ha instalado Ajax. Completado sales.html.

---
+++ Front/dist/js/testHttpRequest.js
@@ -0,0 +1,20 @@
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+/*Para parsear un JSON hay que hacer JSON.parse(myObject) */
