d7c87c45f7ea7ff9e857e0b3b36ad8c64ce49505
Alejandro Garcia <alexgb11@gmail.com>
2020-11-08 23:39:54 +0100

    Created post/put/delete methods for client

--- Back/src/main/java/net/juanxxiii/services/QueryService.java
+++ Back/src/main/java/net/juanxxiii/services/QueryService.java
@@ -7,9 +7,7 @@ import net.juanxxiii.db.repository.SupplierRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class QueryService {
