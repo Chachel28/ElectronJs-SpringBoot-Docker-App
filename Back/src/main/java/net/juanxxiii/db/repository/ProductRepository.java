5e756320cac7c36b02949b8d51cbfe5122620dc4
Alejandro <alexgb11@gmail.com>
2020-11-10 20:22:37 +0100

    Api actualizada

---
+++ Back/src/main/java/net/juanxxiii/db/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package net.juanxxiii.db.repository;
+
+import net.juanxxiii.db.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
