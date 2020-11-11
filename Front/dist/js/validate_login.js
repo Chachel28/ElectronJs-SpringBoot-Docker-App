4ad73357daba7c7df95d82c9718b278d074fafea
Anibalgomezfer <59883735+Anibalgomezfer@users.noreply.github.com>
2020-11-11 17:34:40 +0100

    Sigo sin sacar la validacion del login

--- Front/dist/js/validate_login.js
+++ Front/dist/js/validate_login.js
@@ -1,14 +1,13 @@
-
-function validation() {
+function Validation(){
     var email  =  document.getElementById("email-login").value;
     var password = document.getElementById("password-login").value;
-    if (email == "admin@gmail.com" && password == "Password") {
-        alert('Login succesfully.');
-        return false;
+
+    if ((email == "admin@gmail.com") && (password == "1234")) {
+        alert('Login successfully');
+        return true;
     }
     else {
-        alert('Wrong email or password');
-
+        alert('Login failed');
+        return false;
     }
-
 }
