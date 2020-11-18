f142b2c4b25aa7bbcd6710bb7abc9460d0a61955
Your Name <you@example.com>
2020-11-18 23:27:20 +0100

    buscando código de Javascript

--- Back/src/main/java/net/juanxxiii/db/entity/Sale.java
+++ Back/src/main/java/net/juanxxiii/db/entity/Sale.java
@@ -32,11 +32,8 @@ public class Sale implements Serializable {
     @JoinColumn(name = "idfactura", referencedColumnName = "idfactura")
     private Receipt receipt;
 
-<<<<<<< HEAD
-    @OneToMany(targetEntity = SaleLine.class, mappedBy = "id", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-=======
+
     @OneToMany(targetEntity = SaleLine.class, mappedBy = "idSale", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
->>>>>>> de2083b0446b5b3adee9d9fbdab1e0f162a8cf26
     private List<SaleLine> saleLines;
 
 }
