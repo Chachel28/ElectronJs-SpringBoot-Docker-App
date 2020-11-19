bb6807dc485b7db177e614f981960a6688b45cba
Your Name <you@example.com>
2020-11-19 22:56:08 +0100

    Agregado Jquery a todas las vistas.

--- Front/dist/js/testHttpRequest.js
+++ Front/dist/js/testHttpRequest.js
@@ -21,32 +21,9 @@ async function apiTestProducts() {
     await fetch('http://localhost:8080/api/v1/products')
   .then(response => response.json())
   .then(response => console.log(response));
+  console.log("hola");
 }
 
-function fetchWithAjaxProducts() {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", 'http://localhost:8080/api/v1/products', true);
-    xmlhttp.onreadystatechange = function() 
-    {
-    if (this.readyState == 4 && this.status == 200) 
-        {
-        var responseJsonObj = JSON.parse(this.responseText);
- 
-        console.log(responseJsonObj.name);
-        console.log(responseJsonObj.description);
-        }
-    };
-    //Si hay cuerpo de la aplicación
-    /* var data = {"name" : "Lokesh"};
-    xmlhttp.send( JSON.stringify( data ) );*/
-    xmlhttp.send();
-    $.ajax({
-        url: 'http://localhost:8080/api/v1/products',
-        contentType: "application/json",
-        dataType: 'json',
-        success: function(result){
-            console.log(result);
-        }
-}
+
 
 /*Para parsear un JSON hay que hacer JSON.parse(myObject) */
