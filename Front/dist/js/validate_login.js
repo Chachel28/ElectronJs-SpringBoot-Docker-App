430b1cf55af8eaa41b72db4cce73d04f9011cd15
Chachel <alexgb11@gmail.com>
2020-11-21 23:53:17 +0100

    added cross origin requests

--- Front/dist/js/validate_login.js
+++ Front/dist/js/validate_login.js
@@ -7,8 +7,8 @@ async function validate_login(){
     let postInit = {
         method: 'POST',
         headers:{            
-            "Content-Type": "application/json",
-            "Accept": "application/json"
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
         },
         body: JSON.stringify(body)
     }
