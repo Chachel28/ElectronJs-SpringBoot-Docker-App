9483419410ac7aeebf408eef2d02cfab218618d3
Chachel28 <alexgb11@gmail.com>
2020-11-24 20:44:42 +0100

    flipas tu, funciona

--- Front/dist/js/salesConection.js
+++ Front/dist/js/salesConection.js
@@ -36,13 +36,13 @@ async function loadSalesList(){
     })
 }
 
-async function loadSaleClient(){
+async function loadSale(){
     const querystring = location.search;
     const params = new URLSearchParams(querystring)
-    let id = params.get("id");
+    let id = params.get('id')
     let idClient = params.get('idClient');
     if(idClient == undefined) idClient = 1;
-    let url = 'http://localhost:8080/api/v1/clients/' + idClient; 
+    let urlCliente = 'http://localhost:8080/api/v1/clients/' + idClient; 
     let getInit = {
         method:'GET',
         headers:{
@@ -50,10 +50,37 @@ async function loadSaleClient(){
             'Accept': 'application/json'
         }
     }
-    console.log(id)
-    await fetch(url, getInit)
+    await fetch(urlCliente, getInit)
+    .then(response => response.json())
+    .then(response => {
+        let name = document.getElementById('NombreCliente')
+        name.innerHTML = name.innerHTML+ response.fullName;
+        let dni = document.getElementById('DniCliente')
+        dni.innerHTML = dni.innerHTML + response.dni;
+        let email = document.getElementById('EmailCliente')
+        email.innerHTML = email.innerHTML + response.email;
+        let iban = document.getElementById('IbanCliente')
+        iban.innerHTML = iban.innerHTML + response.iban;
+        let tele = document.getElementById('TelefonoCliente')
+        tele.innerHTML = tele.innerHTML + response.telephones[0].number;
+        let dire = document.getElementById('DireccionCliente')
+        dire.innerHTML = dire.innerHTML + response.directions[0].direction;
+    })
+    let urlSale = 'http://localhost:8080/api/v1/sales/' + id;
+    await fetch(urlSale, getInit)
     .then(response => response.json())
     .then(response => {
         console.log(response)
+
+        let discount = document.getElementById('descuentoVenta')
+        discount.innerHTML = discount.innerHTML+ response.receipt.discounts;
+        let iva = document.getElementById('ivaVenta')
+        iva.innerHTML = iva.innerHTML + response.receipt.iva + '%';
+        let subtotal = document.getElementById('subtotalVenta')
+        subtotal.innerHTML = subtotal.innerHTML + response.receipt.subtotal;
+        let total = document.getElementById('totalVenta')
+        total.innerHTML = total.innerHTML + response.receipt.total;
+        let date = document.getElementById('fechaVenta')
+        date.innerHTML = date.innerHTML + response.receipt.receiptDate;
     })
 }
